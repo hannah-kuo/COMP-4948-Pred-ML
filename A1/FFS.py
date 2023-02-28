67b941d9b035e6f50fcfffa0b75cf9f6393e1ccf
hannah-kuo <hannahkuo0205@gmail.com>
2023-02-27 17:49:00 -0800

    A1 - FFS using Pat's format

--- A1/FFS.py
+++ A1/FFS.py
@@ -1,10 +1,9 @@
 import pandas as pd
-import numpy as np
 from sklearn.impute import KNNImputer
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_selection import f_regression
-from sklearn.metrics import accuracy_score
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 # read the cleaned dataset
 df = pd.read_csv('cleaned_dataset.csv')
@@ -21,27 +20,13 @@ y = df_imputed['Sepsis_Positive']
 lr = LogisticRegression(max_iter=10000)
 
 # forward feature selection with f_regression
-features = []
-scores = []
-for i in range(len(X.columns)):
-    best_score = 0
-    best_feature = ""
-    for col in X.columns:
-        if col not in features:
-            new_features = features + [col]
-            X_new = X[new_features]
-            # f_regression returns F statistic for each feature.
-            f_scores, _ = f_regression(X_new, y)
-            lr.fit(X_new, y)
-            y_pred = lr.predict(X_new)
-            score = accuracy_score(y, y_pred) * f_scores[i]
-            if score > best_score:
-                best_score = score
-                best_feature = col
-    features.append(best_feature)
-    scores.append(best_score)
-    print("Added feature:", best_feature, "Score:", best_score)
+# f_regression returns F statistic for each feature.
+ffs = f_regression(X, y)
 
-# print selected features and their scores
-print("Selected Features:", features)
-print("Feature Scores:", scores)
+featuresDf = pd.DataFrame()
+for i in range(0, len(X.columns)):
+    featuresDf = featuresDf.append({"feature": X.columns[i],
+                                    "ffs": ffs[0][i]}, ignore_index=True)
+featuresDf = featuresDf.sort_values(by=['ffs'], ascending=False)
+print("\nSignificant features in descending F-statistic values:")
+print(featuresDf)
