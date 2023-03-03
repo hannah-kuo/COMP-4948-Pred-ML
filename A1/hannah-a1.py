496d7763f1964e3521990bb62dbabe487b130491
hannah-kuo <hannahkuo0205@gmail.com>
2023-03-02 23:46:28 -0800

    A1 - submission code added ensemble model

--- A1/hannah-a1.py
+++ A1/hannah-a1.py
@@ -23,6 +23,15 @@ from sklearn.svm import SVC
 from sklearn.metrics import classification_report, accuracy_score, precision_score, recall_score, f1_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.exceptions import FitFailedWarning
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split, KFold
+from sklearn.metrics import classification_report, accuracy_score, precision_score, recall_score, f1_score
+from sklearn.linear_model import LogisticRegression
+from mlxtend.classifier import EnsembleVoteClassifier
+from xgboost import XGBClassifier
+from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier
+from sklearn.preprocessing import StandardScaler
 import warnings
 
 from sklearn.preprocessing import StandardScaler
@@ -270,6 +279,8 @@ print("F1 std: " + str(np.std(f1List)))
 # Grid search for optimal hyperparameter tuning
 # --------------------------------------------------------------
 
+print("\n\n ----- Finding the best hyper-parameters to perform tuning for the classifiers/models later ----- ")
+
 # Initialize K-Fold cross-validation
 k_fold = KFold(n_splits=3, shuffle=True)
 
@@ -388,7 +399,6 @@ print(f"Best hyperparameters for XGBClassifier: {grid_search.best_params_}")
 
 """ Bagged Model with Cross Fold Validation, Hyperparameter Tuning, and Scaling"""
 
-
 print("\n\n ----- Bagged Model with Cross Fold Validation, Hyperparameter Tuning, and Scaling -----")
 # Create classifiers
 knn = KNeighborsClassifier()
@@ -455,11 +465,79 @@ for clf in classifierArray:
     # max_features means the maximum number of features to draw from X.
     # max_samples sets the percentage of available data used for fitting.
     # did hyperparameter tuning for "max_features" and "n_estimators"
-    # max_samples = 0.4, 0.1
-    bagging_clf = BaggingClassifier(clf, max_samples=0.1, max_features=5, n_estimators=1000)
+    bagging_clf = BaggingClassifier(clf, max_samples=0.4, max_features=5, n_estimators=1000)
     baggedModel = bagging_clf.fit(X_train, y_train)
     evaluateModel(baggedModel, X_test, y_test, "BAGGED: " + modelType)
 
 """ Ensemble Model with Cross Fold Validation, Hyperparameter Tuning, and Scaling"""
+
+print("\n\n ----- Ensemble Model with Cross Fold Validation, Hyperparameter Tuning, and Scaling -----")
+
+X = df[['PL', 'M11', 'BD2', 'PRG', 'Age']]
+
+# Scale the predictor variables using StandardScaler
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
+# with tuned hyperparemeter
+ada_boost = AdaBoostClassifier(learning_rate=0.1, n_estimators=200)
+grad_boost = GradientBoostingClassifier(learning_rate=0.01, max_depth=7, subsample=1.0)
+xgb_boost = XGBClassifier(learning_rate=0.01, max_depth=3, n_estimators=500)
+eclf = EnsembleVoteClassifier(clfs=[ada_boost, grad_boost, xgb_boost], voting='hard')
+lr = LogisticRegression(C=1, penalty='l2', max_iter=100000)
+
+# Build array of classifiers.
+classifiers = [ada_boost, grad_boost, xgb_boost, eclf, lr]
+
+# Set up KFold cross-validation
+kf = KFold(n_splits=10, shuffle=True)
+
+# Loop through the classifiers and perform cross-validation
+for clf in classifiers:
+    print()
+    print(clf.__class__.__name__)
+    # Initialize variables for storing the results across folds
+    all_predictions = []
+    all_true_labels = []
+    all_accuracy_scores = []
+    all_precision_scores = []
+    all_recall_scores = []
+    all_f1_scores = []
+    for train_index, test_index in kf.split(X):
+        # Split the data into train and test sets for this fold
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+        # Train the classifier on the training set for this fold
+        clf.fit(X_train, y_train)
+        # Use the classifier to make predictions on the test set for this fold
+        predictions = clf.predict(X_test)
+        # Append the predictions and true labels to the running lists
+        all_predictions.extend(predictions)
+        all_true_labels.extend(y_test)
+        # Compute the evaluation metrics for this fold
+        accuracy = accuracy_score(y_test, predictions)
+        precision = precision_score(y_test, predictions)
+        recall = recall_score(y_test, predictions)
+        f1 = f1_score(y_test, predictions)
+        all_accuracy_scores.append(accuracy)
+        all_precision_scores.append(precision)
+        all_recall_scores.append(recall)
+        all_f1_scores.append(f1)
+
+    # Compute the average evaluation metrics across all folds for this classifier
+    avg_accuracy = np.mean(all_accuracy_scores)
+    std_accuracy = np.std(all_accuracy_scores)
+    avg_precision = np.mean(all_precision_scores)
+    std_precision = np.std(all_precision_scores)
+    avg_recall = np.mean(all_recall_scores)
+    std_recall = np.std(all_recall_scores)
+    avg_f1 = np.mean(all_f1_scores)
+    std_f1 = np.std(all_f1_scores)
+    # Print the evaluation metrics for this classifier
+    print("\nAverage evaluation metrics over cross fold validation folds:")
+    print(f"Accuracy:\tmean: {avg_accuracy} \tstd: {std_accuracy}")
+    print(f"Precision: \t{avg_precision} \tstd: {std_precision}")
+    print(f"Recall: \tmean: {avg_recall} \ttsd: {std_recall}")
+    print(f"F1-score: \t{avg_f1} \tstd: {std_f1}")
 
 """ Stacked Model with Cross Fold Validation, Hyperparameter Tuning, and Scaling"""
