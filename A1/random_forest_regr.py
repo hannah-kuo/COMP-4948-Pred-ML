66035e6c6d4b8bfd356a4d3f5325c9dd6ad39442
hannah-kuo <hannahkuo0205@gmail.com>
2023-03-02 18:22:08 -0800

    A1 - random forest regressor

---
+++ A1/random_forest_regr.py
@@ -0,0 +1,58 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
+import warnings
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+# Load the dataset into a pandas DataFrame object
+df = pd.read_csv('cleaned_dataset.csv')
+
+# Split the dataset into a target variable and predictor variables
+y = df['Sepsis_Positive']
+X = df.drop(['Sepsis_Positive'], axis=1)
+feature_list = ['PRG', 'PL', 'PR', 'SK', 'TS', 'M11', 'BD2', 'Age', 'Insurance']
+
+# Split dataset into training set and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, train_size=0.7)
+
+# Use the random grid to search for best hyperparameters
+rf = RandomForestRegressor()
+random_grid = \
+    {'bootstrap': [True],
+     'max_depth': [4, 6, None],
+     'max_features': ['auto'],
+     'min_samples_leaf': [15],
+     'min_samples_split': [15],
+     'n_estimators': [400, 800, 1600]}
+
+rf_random = RandomizedSearchCV(estimator=rf, param_distributions=random_grid, n_iter=100, cv=3, n_jobs=-1)
+# Fit the random search model
+rf_random.fit(X_train, y_train)
+print("Best parameters to use for random forest")
+print(rf_random.best_params_)
+print("----------------------------------------")
+
+# We create the new rf with the best random forest params suggested above
+rf = RandomForestRegressor(n_estimators=800, min_samples_split=15, min_samples_leaf=15, max_features='auto',
+                           max_depth=6, bootstrap=True)
+rf.fit(X_train, y_train)
+
+# Find feature importance's
+importances = list(rf.feature_importances_)
+
+
+# Present features and importance scores.
+def showFeatureImportances(importances, feature_list):
+    dfImportance = pd.DataFrame()
+    for i in range(0, len(importances)):
+        dfImportance = dfImportance.append({"importance": importances[i],
+                                            "feature": feature_list[i]},
+                                           ignore_index=True)
+
+    dfImportance = dfImportance.sort_values(by=['importance'],
+                                            ascending=False)
+    print(dfImportance)
+
+
+showFeatureImportances(importances, X_train.columns)
