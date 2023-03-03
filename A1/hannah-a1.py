165823c58820d5cecb40af100bf3d0bb1bbca8d2
hannah-kuo <hannahkuo0205@gmail.com>
2023-03-02 23:54:10 -0800

    A1 - submission code added stacked model

--- A1/hannah-a1.py
+++ A1/hannah-a1.py
@@ -1,40 +1,32 @@
 from sklearn.feature_selection import RFE
 from sklearn.feature_selection import f_regression
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import train_test_split, RandomizedSearchCV
+from sklearn.model_selection import RandomizedSearchCV
 from sklearn import metrics
-import numpy as np
-from sklearn.metrics import classification_report, roc_auc_score
+from sklearn.metrics import roc_auc_score
 from sklearn.metrics import average_precision_score
-import pandas as pd
-from sklearn.impute import KNNImputer
-from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split, cross_val_score, KFold
-from xgboost import XGBClassifier
-from sklearn.model_selection import GridSearchCV
-import pandas as pd
 from sklearn.impute import KNNImputer
-from sklearn.model_selection import train_test_split, KFold, cross_val_score
+from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import BaggingClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import RidgeClassifier
 from sklearn.svm import SVC
-from sklearn.metrics import classification_report, accuracy_score, precision_score, recall_score, f1_score
-from sklearn.linear_model import LogisticRegression
 from sklearn.exceptions import FitFailedWarning
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split, KFold
 from sklearn.metrics import classification_report, accuracy_score, precision_score, recall_score, f1_score
-from sklearn.linear_model import LogisticRegression
 from mlxtend.classifier import EnsembleVoteClassifier
 from xgboost import XGBClassifier
-from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier
-from sklearn.preprocessing import StandardScaler
-import warnings
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
+from sklearn.tree import DecisionTreeClassifier
 
 from sklearn.preprocessing import StandardScaler
+import warnings
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 warnings.filterwarnings("ignore", category=FitFailedWarning)
@@ -279,7 +271,8 @@ print("F1 std: " + str(np.std(f1List)))
 # Grid search for optimal hyperparameter tuning
 # --------------------------------------------------------------
 
-print("\n\n ----- Finding the best hyper-parameters to perform tuning for the classifiers/models later ----- ")
+print("\n\n ----- Finding the best hyperparameters to perform hyperparameter tuning "
+      "for the classifiers/models later ----- \n")
 
 # Initialize K-Fold cross-validation
 k_fold = KFold(n_splits=3, shuffle=True)
@@ -307,7 +300,6 @@ rf_grid_search.fit(X_train, y_train)
 
 # Print the best hyperparameters and F1 score for RandomForestClassifier
 print("Best hyperparameters for RandomForestClassifier:", rf_grid_search.best_params_)
-print("Best F1 score for RandomForestClassifier:", rf_grid_search.best_score_)
 
 # Create a GridSearchCV object for XGBClassifier
 xgb_grid_search = GridSearchCV(XGBClassifier(), xgb_param_grid, cv=k_fold, scoring='f1', error_score='raise')
@@ -317,10 +309,6 @@ xgb_grid_search.fit(X_train, y_train)
 
 # Print the best hyperparameters and F1 score for XGBClassifier
 print("Best hyperparameters for XGBClassifier:", xgb_grid_search.best_params_)
-print("Best F1 score for XGBClassifier:", xgb_grid_search.best_score_)
-
-from sklearn.model_selection import GridSearchCV
-from sklearn.linear_model import LogisticRegression
 
 # Define parameter grid for LogisticRegression
 param_grid_lr = {'C': [0.001, 0.01, 0.1, 1, 10, 100],
@@ -336,9 +324,6 @@ grid_search_lr.fit(X_train, y_train)
 # Print best hyperparameters
 print("Best hyperparameters for LogisticRegression:", grid_search_lr.best_params_)
 
-from sklearn.model_selection import GridSearchCV
-from sklearn.tree import DecisionTreeClassifier
-
 # Define parameter grid for DecisionTreeClassifier
 param_grid_dt = {'max_depth': [3, 5, 7, 9, 11],
                  'min_samples_split': [2, 5, 10],
@@ -353,10 +338,6 @@ grid_search_dt.fit(X_train, y_train)
 
 # Print best hyperparameters
 print("Best hyperparameters for DecisionTreeClassifier:", grid_search_dt.best_params_)
-
-from sklearn.model_selection import GridSearchCV
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import AdaBoostClassifier
 
 # Define parameter grid for AdaBoostClassifier
 param_grid_ab = {'n_estimators': [50, 100, 200],
@@ -375,8 +356,6 @@ grid_search_ab.fit(X_train, y_train)
 
 # Print best hyperparameters
 print("Best hyperparameters for AdaBoostClassifier:", grid_search_ab.best_params_)
-
-from sklearn.model_selection import GridSearchCV
 
 # Define the parameter grid
 param_grid = {
@@ -541,3 +520,88 @@ for clf in classifiers:
     print(f"F1-score: \t{avg_f1} \tstd: {std_f1}")
 
 """ Stacked Model with Cross Fold Validation, Hyperparameter Tuning, and Scaling"""
+
+# Initialize K-Fold cross-validation
+k_fold = KFold(n_splits=3, shuffle=True)
+
+
+def getUnfitModels():
+    models = list()
+
+    # models.append(LogisticRegression(max_iter=10000000))
+    models.append(LogisticRegression(C=1, penalty='l2'))
+    models.append(DecisionTreeClassifier(max_depth=11, min_samples_leaf=2, min_samples_split=10))
+    models.append(AdaBoostClassifier())
+    # tuned the hyperparameter n_estimators=800 from what was suggested in the best parameters to use for random forest
+    models.append(RandomForestClassifier(max_depth=10, min_samples_leaf=1, min_samples_split=5, n_estimators=800))
+
+    models.append(GradientBoostingClassifier(learning_rate=0.01, max_depth=7, subsample=1.0))
+    models.append(XGBClassifier())
+    return models
+
+
+def evaluateStackedModel(y_test, predictions, model):
+    # Calculate evaluation metrics
+    print("\nAverage evaluation metrics over cross fold validation folds:")
+    acc_scores = cross_val_score(model, X, y, cv=k_fold, scoring='accuracy')
+    precision_scores = cross_val_score(model, X, y, cv=k_fold, scoring='precision')
+    recall_scores = cross_val_score(model, X, y, cv=k_fold, scoring='recall')
+    f1_scores = cross_val_score(model, X, y, cv=k_fold, scoring='f1')
+    # Print evaluation metrics
+    showStats("Accuracy", acc_scores)
+    showStats("Precision", precision_scores)
+    showStats("Recall", recall_scores)
+    showStats("F1 Score", f1_scores)
+
+
+def fitBaseModels(X_train, y_train, X_test, models):
+    dfPredictions = pd.DataFrame()
+
+    # Fit base model and store its predictions in dataframe.
+    for i in range(0, len(models)):
+        models[i].fit(X_train, y_train)
+        predictions = models[i].predict(X_test)
+        colName = str(i)
+        dfPredictions[colName] = predictions
+    return dfPredictions, models
+
+
+def fitStackedModel(X, y):
+    model = LogisticRegression()
+    model.fit(X, y)
+    return model
+
+
+# Create a KNN imputer object with n_neighbors=5
+imputer = KNNImputer(n_neighbors=5)
+
+# Impute the missing values in the predictor variables
+X_imputed = imputer.fit_transform(X)
+
+# Scale the data using StandardScaler
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X_imputed)
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.3)
+
+# Get base models.
+unfitModels = getUnfitModels()
+
+# Fit base and stacked models.
+dfPredictions, models = fitBaseModels(X_train, y_train, X_test, unfitModels)
+stackedModel = fitStackedModel(dfPredictions, y_test)
+
+# Evaluate base models with validation data.
+print("\n---------- Evaluate Base Models ----------")
+for i in range(0, len(models)):
+    scores = cross_val_score(models[i], X_test, y_test, cv=5)
+    print("\n*** " + models[i].__class__.__name__)
+    predictions = models[i].predict(X_test)
+    evaluateStackedModel(y_test, predictions, models[i])
+
+# Evaluate stacked model with validation data.
+print("\n---------- Evaluate Stacked Model ----------")
+scores = cross_val_score(stackedModel, dfPredictions, y_test, cv=5)
+stackedPredictions = stackedModel.predict(dfPredictions)
+evaluateStackedModel(y_test, stackedPredictions, stackedModel)
