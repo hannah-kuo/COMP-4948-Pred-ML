9568d63ef45c9a3c0286c111e9e6abcb8764d32f
hannah-kuo <hannahkuo0205@gmail.com>
2023-03-02 23:43:08 -0800

    A1 - submission code added bagged model

--- A1/hannah-a1.py
+++ A1/hannah-a1.py
@@ -13,11 +13,23 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split, cross_val_score, KFold
 from xgboost import XGBClassifier
 from sklearn.model_selection import GridSearchCV
+import pandas as pd
+from sklearn.impute import KNNImputer
+from sklearn.model_selection import train_test_split, KFold, cross_val_score
+from sklearn.ensemble import BaggingClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import RidgeClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report, accuracy_score, precision_score, recall_score, f1_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.exceptions import FitFailedWarning
 import warnings
 
 from sklearn.preprocessing import StandardScaler
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.filterwarnings("ignore", category=FitFailedWarning)
+warnings.filterwarnings("ignore")
 
 """ Data Cleaning """
 
@@ -109,9 +121,7 @@ print("\n\n*** Forward Feature Selection")
 print("\nSignificant features in descending F-statistic values:")
 print(featuresDf)
 
-
 """ Feature Selection - Random Forest """
-
 
 # Split the dataset into a target variable and predictor variables
 y = df['Sepsis_Positive']
@@ -377,6 +387,78 @@ grid_search.fit(X_train, y_train)
 print(f"Best hyperparameters for XGBClassifier: {grid_search.best_params_}")
 
 """ Bagged Model with Cross Fold Validation, Hyperparameter Tuning, and Scaling"""
+
+
+print("\n\n ----- Bagged Model with Cross Fold Validation, Hyperparameter Tuning, and Scaling -----")
+# Create classifiers
+knn = KNeighborsClassifier()
+svc = SVC()
+rg = RidgeClassifier()
+# lr = LogisticRegression(fit_intercept=True, solver='liblinear')
+lr = LogisticRegression(max_iter=10000000, random_state=42)
+
+# Build array of classifiers.
+classifierArray = [knn, svc, rg, lr]
+
+# Initialize K-Fold cross-validation
+k_fold = KFold(n_splits=3, shuffle=True)
+
+
+def showStats(classifier, scores):
+    print(classifier + ":    ", end="")
+    strMean = str(round(scores.mean(), 2))
+    strStd = str(round(scores.std(), 2))
+    print("Mean: " + strMean + "   ", end="")
+    print("Std: " + strStd)
+
+
+# Create a KNN imputer object with n_neighbors=5
+imputer = KNNImputer(n_neighbors=5)
+
+# Impute the missing values in the predictor variables
+X_imputed = imputer.fit_transform(X)
+
+# Scale the data using StandardScaler
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X_imputed)
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.3)
+
+
+def evaluateModel(model, X_test, y_test, title):
+    print("\n\n*** " + title + " ***")
+    predictions = model.predict(X_test)
+    report = classification_report(y_test, predictions)
+    # print(report)
+    # Calculate evaluation metrics
+    print("-- Average evaluation metrics over cross fold validation folds --")
+    acc_scores = cross_val_score(model, X, y, cv=k_fold, scoring='accuracy')
+    precision_scores = cross_val_score(model, X, y, cv=k_fold, scoring='precision')
+    recall_scores = cross_val_score(model, X, y, cv=k_fold, scoring='recall')
+    f1_scores = cross_val_score(model, X, y, cv=k_fold, scoring='f1')
+    # Print evaluation metrics
+    showStats("Accuracy", acc_scores)
+    showStats("Precision", precision_scores)
+    showStats("Recall", recall_scores)
+    showStats("F1 Score", f1_scores)
+
+
+# Search for the best classifier.
+for clf in classifierArray:
+    modelType = clf.__class__.__name__
+
+    # Create and evaluate stand-alone model.
+    clfModel = clf.fit(X_train, y_train)
+    evaluateModel(clfModel, X_test, y_test, modelType)
+
+    # max_features means the maximum number of features to draw from X.
+    # max_samples sets the percentage of available data used for fitting.
+    # did hyperparameter tuning for "max_features" and "n_estimators"
+    # max_samples = 0.4, 0.1
+    bagging_clf = BaggingClassifier(clf, max_samples=0.1, max_features=5, n_estimators=1000)
+    baggedModel = bagging_clf.fit(X_train, y_train)
+    evaluateModel(baggedModel, X_test, y_test, "BAGGED: " + modelType)
 
 """ Ensemble Model with Cross Fold Validation, Hyperparameter Tuning, and Scaling"""
 
