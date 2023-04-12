115bc5f0863879372661e11f6c878377013566fe
hannah-kuo <hannahkuo0205@gmail.com>
2023-04-12 14:01:16 -0700

    Final Q6 with only val loss eval metric

---
+++ Final/Q6.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.callbacks import EarlyStopping
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_absolute_error, r2_score
+
+data = pd.read_csv('C:/PredML/petrol_consumption.csv')
+
+X = data.iloc[:, 0:4].values
+y = data.iloc[:, 4].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+model = Sequential()
+model.add(Dense(100, activation='relu', input_dim=4))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(1))
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+early_stopping = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
+
+history = model.fit(X_train, y_train, epochs=100, batch_size=32, validation_split=0.2, callbacks=[early_stopping])
+
+train_loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+plt.plot(train_loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
